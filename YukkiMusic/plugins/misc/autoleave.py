c48fee4276cc475ba32430ead044f9f9bbc72467
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-07-04 17:15:51 +0800

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -26,6 +26,7 @@ async def auto_leave():
 
             for num in assistants:
                 client = await get_client(num)
+                left = 0
                 try:
                     async for i in client.iter_dialogs():
                         chat_type = i.chat.type
@@ -37,15 +38,18 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
                                 and chat_id != -1001234685537
+                                and chat_id != -1001626676042
                                 and chat_id != -1001616118955
                             ):
+                                if left == 20:
+                                    continue
                                 if not await is_active_chat(chat_id):
                                     try:
                                         await client.leave_chat(
                                             chat_id
                                         )
+                                        left += 1
                                     except:
                                         continue
                 except:
